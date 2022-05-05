01c35b1cb6abedec5a9ef19c4d3d03a1f373b114
Julia Grib <yugrib.jobs@gmail.com>
2022-05-05 19:09:20 +0300

    add all

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const burgerBtn = document.querySelector('.header-burger');
+const leftMenu = document.querySelector('.left-menu');
+const space = document.querySelector('.header-burger-space');
+const burherLink = document.querySelectorAll('.header-menu-burger__link');
+const html = document.querySelector('html');
+
+
 
 burgerBtn.addEventListener('click', function(){
 	burgerBtn.classList.toggle('active');
+	leftMenu.classList.toggle("left-menu__active");
+	html.classList.toggle("overflow-h");
+	space.classList.toggle("active-space");
 })
+
